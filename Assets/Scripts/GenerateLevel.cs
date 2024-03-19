f8fef88db4b239358971706064b6606a88e6bae2
Corey <coreyshanahan01@gmail.com>
2024-03-19 17:05:28 +0000

    level gen

--- Assets/Scripts/GenerateLevel.cs
+++ Assets/Scripts/GenerateLevel.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class GenerateLevel : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject _verticalWall, _horizontalWall, _player, _npc;
+
     void Start()
     {
-        
+        GenerateFromFile();
     }
 
-    // Update is called once per frame
-    void Update()
+    void GenerateFromFile()
     {
-        
+        TextAsset t1 = (TextAsset)Resources.Load("maze", typeof(TextAsset));
+        string map = t1.text.Replace(System.Environment.NewLine, "");
+
+        for (int i = 0; i < map.Length; i++)
+        {
+            int column, row;
+            column = i % 10;
+            row = i / 10;
+
+            if (map[i] == '1') Instantiate(_verticalWall, new Vector3(50 - column * 10, 1.5f, 50 - row * 10), Quaternion.identity);
+            // else if (map[i] == '2') Instantiate(_npc, new Vector3(50 - column * 10, 1.5f, 50 - row * 10), Quaternion.identity);
+        }
     }
 }
